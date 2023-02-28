cda29d00c1945e9406ecaa8fa9ce8e039543d11d
Hana <grace.sambora@gmail.com>
2023-02-27 20:46:19 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000);
+console.log(random);
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3%num4;
+
+const max = Math.max(3, 14, 0, 20, 7);
